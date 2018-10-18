5ea384754297c49171e1d015f45e93ead49fc4cc
erikh <erikh@hig.no>
2018-10-18 09:52:11 +0200

    fixed upgrade to 1.12.1 and 3.2.24

--- site/profile/manifests/kubernetes/worker.pp
+++ site/profile/manifests/kubernetes/worker.pp
@@ -11,7 +11,10 @@ class profile::kubernetes::worker {
 #    docker_key_source   => 'https://download.docker.com/linux/ubuntu/gpg',
 #    kubernetes_apt_release => "kubernetes-xenial",
 #    kubernetes_version     => '1.12.1',
+#    kubernetes_package_version     => '1.12.1-00',
 #    etcd_version           => '3.2.24',
+#    etcd_archive           => 'etcd-v3.2.24-linux-amd64.tar.gz',
+#    etcd_source            => 'https://github.com/coreos/etcd/releases/download/v3.2.24/etcd-v3.2.24-linux-amd64.tar.gz',
 #  }
 
 }
